fe00d878a45e5bbd8bef63a510e390c7c4509425
Kat B <katreesebarnes@gmail.com>
2019-01-03 19:37:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
 return `Happy holidays, ${name}!`
 } 
-function happyHolidaysTo(holiday, name) {
+function happyHolidayTo(holiday, name) {
 return `Happy ${holiday}, ${name}!`
   
 }
