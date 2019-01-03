6d60f3eb9467dba1247a2f20548da6488dce92cc
Kat B <katreesebarnes@gmail.com>
2019-01-03 19:42:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ function happyHolidayTo(holiday, name) {
 return `Happy ${holiday}, ${name}!`
   
 }
-
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
 
    
   
